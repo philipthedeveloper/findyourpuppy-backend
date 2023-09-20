fcbe3f917314bb5292893e07bbb09b9fb726762f
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-20 08:36:12 +0100

    Create Custom Error Classes

---
+++ errors/index.js
@@ -0,0 +1,7 @@
+export * from "./BadRequest.js";
+export * from "./Conflict.js";
+export * from "./CustomError.js";
+export * from "./MethodNotAllowedd.js";
+export * from "./NotFound.js";
+export * from "./Unauthorized.js";
+export * from "./UnprocessableEntity.js";
