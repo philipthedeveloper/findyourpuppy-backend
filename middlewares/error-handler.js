7726a862a51621334a89cf6a481975e35a90e2f2
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-20 08:15:09 +0100

    Update errorHandler middleware to handle mongodb validation error

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -3,6 +3,10 @@ import { StatusCodes, ReasonPhrases } from "http-status-codes";
 const errorHandler = (err, req, res, next) => {
   let errorObject = {};
   console.log(err);
+  if (err && err.name === "ValidationError") {
+    errorObject.status = StatusCodes.BAD_REQUEST;
+    errorObject.message = err.message;
+  }
   let status = errorObject?.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   return res.status(status).json({
     success: false,
