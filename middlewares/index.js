4e316bd1d1c23be5c607df37bd7b24728735cb06
philipthedeveloper <philipowolabi79@gmail.com>
2023-09-21 18:39:19 +0100

    Export validate JWT middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,5 +2,12 @@ import methodChecker from "./method-checker.js";
 import requestLogger from "./req-logger.js";
 import routeNotFound from "./route-not-found.js";
 import errorHandler from "./error-handler.js";
+import validateToken from "./validate-jwt.js";
 
-export { requestLogger, methodChecker, routeNotFound, errorHandler };
+export {
+  requestLogger,
+  methodChecker,
+  routeNotFound,
+  errorHandler,
+  validateToken,
+};
